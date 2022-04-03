f41cf384d39d0dec0819684f0755f5b5dd4ed58f
Dyrits <61299170+Dyrits@users.noreply.github.com>
2022-04-03 13:07:13 -0500

    Class ".active-button" corrected to ".active-btn'

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 (function () {
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
-            document.querySelector(".active-button").classList.remove("active-button");
-            this.classList.add("active-button");
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
             document.querySelector(".active").classList.remove("active");
             document.getElementById(button.dataset.id).classList.add("active");
         })
