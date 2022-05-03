434b79378c7761339b06e02f5bf0da1ef1b001a2
Dyrits <dyrits@outlook.com>
2022-05-03 12:46:37 -0500

    Minor refactoring
    Diffferent partials files have been created and imported and the main stylesheet.
    The sidebar has been set as an aside element. Some modificaitons have been made, and the script has been updated.
    The BEM methodology have been used with unique ID used as CSS selector, for the header section.
    The stylesheet has been cleaned for the header section.

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 (function () {
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
-            document.querySelector(".active-btn").classList.remove("active-btn");
-            this.classList.add("active-btn");
+            document.querySelector(".active-button").classList.remove("active-button");
+            this.classList.add("active-button");
             document.querySelector(".active").classList.remove("active");
             document.getElementById(button.dataset.id).classList.add("active");
         })
     });
-    document.querySelector(".theme-btn").addEventListener("click", () => {
+    document.querySelector("#switch-mode").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
     })
 })();
