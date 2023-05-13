ae509e7a8cdebd907cc48be0cda6af52969fe71d
Mateusz <61660055+yoggys@users.noreply.github.com>
2023-05-14 01:55:06 +0200

    add hash navigation

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-(function () {
-    [...document.querySelectorAll(".control")].forEach(button => {
-        button.addEventListener("click", function() {
-            document.querySelector(".active-btn").classList.remove("active-btn");
-            this.classList.add("active-btn");
-            document.querySelector(".active").classList.remove("active");
-            document.getElementById(button.dataset.id).classList.add("active");
-        })
-    });
-    document.querySelector(".theme-btn").addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
-    })
-})();
+function makeActive(id) {
+  if (id) location.hash = "#" + id;
+  const hash = location.hash;
+  if (hash) {
+    document.querySelector(".active-btn").classList.remove("active-btn");
+    document.querySelector(".active").classList.remove("active");
+    document
+      .querySelector(`[data-id="${hash.slice(1)}"]`)
+      .classList.add("active-btn");
+    document.querySelector(hash).classList.add("active");
+  }
+}
+
+[...document.querySelectorAll(".control")].forEach((button) => {
+  button.addEventListener("click", function () {
+    makeActive(button.dataset.id);
+  });
+});
+document.querySelector(".theme-btn").addEventListener("click", () => {
+  document.body.classList.toggle("light-mode");
+});
+
+document.onreadystatechange = makeActive();
